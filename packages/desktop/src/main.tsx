74f018d323e594a4016e24e678edb434a84ef530
James Cowx <james@jamescowx.com>
2026-07-29 02:19:12 -0700

    Rename to Nexus Contacts, add landing page, v1.0.0

--- packages/desktop/src/main.tsx
+++ packages/desktop/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from '@contacts/core';
+import { App } from '@nexus/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
